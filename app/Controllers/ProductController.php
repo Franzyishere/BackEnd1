bbd7d6cea570eea04c33660807bf4f2aa09d74d2
Franzyishere <odyfranswijaya@gmail.com>
2023-11-17 22:14:22 +0700

    Rest full API

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -10,19 +10,62 @@ class ProductController extends BaseController {
     public function __construct(){
         $this->product = new ProductModel();
     }
-    public function insertProduct(){
-        if ($this->request->getMethod() == 'post') {
-            $data = [
-                'nama_product' => $this->request->getVar('nama_product'),
-                'description' => $this->request->getVar('description')
-            ];
-    
-            $this->product->insertProductORM($data);
-            return redirect()->to(base_url('products'));
-        } else {
-            return view('tambah_product');
+
+    public function insertProductApi(){
+        $requestData = $this->request->getJSON();
+        $validation = $this->validate([
+            'nama_product' => 'required',
+            'description' => 'required',
+        ]);
+        if (!$validation){
+            $this->response->getStatusCode(400);
+            return $this->response->setJSON([
+                'code' => 400,
+                'status' => 'BAD REQUEST',
+                'data' => 'semua harus terisi'
+            ]);
+        }
+        $data = [
+            'nama_product' => $requestData ->nama_product,
+            'description' => $requestData ->description,
+        ];
+        $insert = $this->product->insertProductORM($data);
+        if ($insert){
+            return $this->respond([
+                'code' => 200,
+                'status' => 'OK',
+                'data' => $data
+            ]);
+        }else{
+            $this->response->setStatusCode(500);
+            return $this->response->setJSON([
+                'code' => 500,
+                'status' => 'INTERNAL SERVER ERROR',
+                'data' =>'null'
+            ]);
         }
     }
+
+    // public function insertProduct(){
+    //     if ($this->request->getMethod() == 'post') {
+    //         $data = [
+    //             'nama_product' => $this->request->getVar('nama_product'),
+    //             'description' => $this->request->getVar('description')
+    //         ];
+    
+    //        $insert = $this->product->insertProductORM($data);
+    //         if ($insert){
+    //             return $this->respond([
+    //                 'code' => 200,
+    //                 'status' => 'OK',
+    //                 'data' => $data
+    //             ]);
+    //         }
+    //         return redirect()->to(base_url('products'));
+    //     } else {
+    //         return view('tambah_product');
+    //     }
+    // }
 
     public function readProduct(){
         $products = $this->product->findAll();
@@ -38,7 +81,7 @@ class ProductController extends BaseController {
         return $this->respond(
             [
                 'code' => 200,
-                'status' => "OK",
+                'status' => 'OK',
                 'data' => $products
             ]
         );
@@ -59,8 +102,8 @@ class ProductController extends BaseController {
             $this->response->setStatusCode(404);
             return $this->response->setJSON([
                 'code' => 404,
-                'status' => "NOT FOUND",
-                'data' => "product tidak ditemukan"
+                'status' => 'NOT FOUND',
+                'data' => 'product tidak ditemukan'
             ]);
         }
         return $this->respond(
